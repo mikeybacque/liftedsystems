b324742cb24b5a8df5f851f28f39a1c8618f5322
mikeybacque <mikey.bacque@gmail.com>
2017-05-03 13:16:29 -0400

    waypoints

--- js/main.js
+++ js/main.js
@@ -14,4 +14,54 @@ var $contactfade = $('.contactfade');
 
 $contactfade.waypoint(function () {
   $('.contactfade').addClass('animate');
+}, { offset: '60%' });
+
+
+
+var $history = $('.history');
+
+$history.waypoint(function () {
+  $('.1').addClass('orange');
+}, { offset: '50%' });
+
+$history.waypoint(function () {
+  $('.2').removeClass('orange');
+  $('.3').removeClass('orange');
+}, { offset: '100%' });
+
+
+var $projects = $('.projects');
+
+$projects.waypoint(function () {
+  $('.2').addClass('orange');
 }, { offset: '70%' });
+
+
+$projects.waypoint(function () {
+  $('.1').removeClass('orange');
+  $('.3').removeClass('orange');
+}, { offset: '100%' });
+
+
+var $contactfade = $('.contactfade');
+
+$contactfade.waypoint(function () {
+  $('.3').addClass('orange');
+
+}, { offset: '60%' });
+
+$contactfade.waypoint(function () {
+  $('.2').removeClass('orange');
+  $('.1').removeClass('orange');
+}, { offset: '100%' });
+
+
+
+
+var $banner = $('.banner');
+
+$banner.waypoint(function () {
+  $('.2').removeClass('orange');
+  $('.1').removeClass('orange');
+  $('.3').removeClass('orange');
+}, { offset: '-20%' });
