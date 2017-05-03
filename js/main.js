44cbf71c0a8bab3f8c5b4c0001db2bb2be703caa
mikeybacque <mikey.bacque@gmail.com>
2017-05-03 13:00:11 -0400

    waypoint transitions

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var $history = $('.history');
+
+$history.waypoint(function () {
+  $('.history').addClass('animate');
+}, { offset: '70%' });
+
+var $projects = $('.projects');
+
+$projects.waypoint(function () {
+  $('.projects').addClass('animate');
+}, { offset: '70%' });
+
+var $contactfade = $('.contactfade');
+
+$contactfade.waypoint(function () {
+  $('.contactfade').addClass('animate');
+}, { offset: '70%' });
