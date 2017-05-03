5dc0fa85d23fba67829692b36cbe3cb623798202
mikeybacque <mikey.bacque@gmail.com>
2017-05-03 16:50:46 -0400

    grey footer logo

--- js/main.js
+++ js/main.js
@@ -1,67 +1,111 @@
 var $history = $('.history');
-
-$history.waypoint(function () {
-  $('.history').addClass('animate');
-}, { offset: '70%' });
-
 var $projects = $('.projects');
+var $contactfade = $('.contactfade');
+var $banner = $('.banner');
 
-$projects.waypoint(function () {
-  $('.projects').addClass('animate');
+var $1 = $('.1');
+var $2 = $('.2');
+var $3 = $('.3');
+
+
+
+$history.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.history').addClass('animate');
+  }
 }, { offset: '70%' });
 
-var $contactfade = $('.contactfade');
 
-$contactfade.waypoint(function () {
-  $('.contactfade').addClass('animate');
+
+$projects.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.projects').addClass('animate');
+  }
+}, { offset: '70%' });
+
+
+
+$contactfade.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.contactfade').addClass('animate');
+  }
 }, { offset: '60%' });
 
 
 
-var $history = $('.history');
 
-$history.waypoint(function () {
-  $('.1').addClass('orange');
+
+
+
+$history.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.1').addClass('orange');
+  }
+
+  if (direction == 'up') {
+    $('.1').removeClass('orange');
+    $('.2').removeClass('orange');
+    $('.3').removeClass('orange');
+  }
 }, { offset: '50%' });
 
-$history.waypoint(function () {
-  $('.2').removeClass('orange');
-  $('.3').removeClass('orange');
-}, { offset: '100%' });
 
 
-var $projects = $('.projects');
+$projects.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.2').addClass('orange');
+    $('.1').removeClass('orange');
+    $('.3').removeClass('orange');
+  }
 
-$projects.waypoint(function () {
-  $('.2').addClass('orange');
-}, { offset: '70%' });
+  if (direction == 'up') {
+    $('.2').removeClass('orange');
+  }
+}, { offset: '50%' });
 
 
-$projects.waypoint(function () {
-  $('.1').removeClass('orange');
-  $('.3').removeClass('orange');
-}, { offset: '100%' });
 
+$contactfade.waypoint(function (direction) {
+  if (direction == 'down') {
+    $('.3').addClass('orange');
+    $('.1').removeClass('orange');
+    $('.2').removeClass('orange');
+  }
 
-var $contactfade = $('.contactfade');
-
-$contactfade.waypoint(function () {
-  $('.3').addClass('orange');
-
+  if (direction == 'up') {
+    $('.3').removeClass('orange');
+  }
 }, { offset: '60%' });
 
-$contactfade.waypoint(function () {
-  $('.2').removeClass('orange');
-  $('.1').removeClass('orange');
-}, { offset: '100%' });
 
 
 
-
-var $banner = $('.banner');
 
 $banner.waypoint(function () {
   $('.2').removeClass('orange');
   $('.1').removeClass('orange');
   $('.3').removeClass('orange');
 }, { offset: '-20%' });
+
+
+
+
+
+
+$1.on('click', function () {
+  $('.1').addClass('orange');
+  $('.2').removeClass('orange');
+  $('.3').removeClass('orange');
+});
+
+$2.on('click', function () {
+  $('.2').addClass('orange');
+  $('.1').removeClass('orange');
+  $('.3').removeClass('orange');
+});
+
+$3.on('click', function () {
+  $('.3').addClass('orange');
+  $('.2').removeClass('orange');
+  $('.1').removeClass('orange');
+});
